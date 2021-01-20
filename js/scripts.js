e615b7570efcb4fd2bd8f2db97637032f45bc998
Kelly Young <kkristineyoung@gmail.com>
2021-01-19 19:47:35 -0500

    add color wheel form

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
   const beverage = $("#beverage").val();
   const flavor = $("input:radio[name=flavor]:checked".val();
   const dob = $("#born").val();
+  const favoriteColor = $("#color").val();
 
 });
